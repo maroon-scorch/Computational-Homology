33393a98a6cb33084c3db98986340302cf631699
maroon-scorch <matthew_ji@brown.edu>
2022-12-11 07:55:22 -0500

    Lens space computed

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Computational-Homology
+
+## How to Run
+
+To compute the homology of 3-dimensional Lens Spaces:
+```
+python main.py lens <number of tetrahedrons>
+```
+To compute the homology on a custom Delta Complex:
+```
+python main.py <path to file>
+```
+
+## Input Specification
+
+We will explain how the input files should be formatted here, note that you can find some examples under the [example folder](example/).<br/>
+
+The program takes a file of the following format:
+- The first line of the file should be an non-negative integer indicating the number of vertices in the Delta Complex:
+```
+n
+```
+- The proceeding lines are as follows - The $i$-th line of file should contain a sequence of $i$-dimensional simplices, each separated by a semi-colon **;**: 
+```
+<simplex 1>; <simplex 2>; ...; <simplex k>
+```
+- For each simplex on line $i$, it should be represented by a list of indices, where the $j$-th index $v$ of the simplex represents the $j$-th face of the simplex. The index value $v$ means this face is the $v$-th simplex of line $i-1$:
+```
+<index_1> ... <index i>
+```
+For example, the following is the standard Delta Complex specification of the 2-torus:
+```
+1
+0 0; 0 0; 0 0
+0 1 2; 2 1 0
+```
+This structure only has 1 vertex, so all of its 3 edges start and end at the same point. Its two faces go in opposite orientations, hence their edges go in opposite directions.
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import numpy as np
+import numpy as np, sys
 from sympy import Matrix, ZZ
 from sympy.matrices.normalforms import smith_normal_form
+
+def read_input(inputFile):
+    """ Read and parse the input file """
+    with open(inputFile, "r") as f:
+        number_of_points = int(f.readline())
+        points = [() for _ in range(number_of_points)]
+        
+        input = [points]
+        for line in f.readlines():
+            tokens = line.strip().split(";")
+            dim_list = []
+            for elt in tokens:
+                t = [int(e) for e in elt.strip().split()]
+                dim_list.append(t)
+            input.append(dim_list) 
+    return input
 
 def normal_basis(n, k):
     """ Returns e_k of the rank n free Z-module """
@@ -37,8 +53,8 @@ def boundary_map(prev, next):
             boundary_map.append(column_vector)
         
         boundary_map = np.column_stack(boundary_map)
-        print(boundary_map)
-        print(np.shape(boundary_map))
+        # print(boundary_map)
+        # print(np.shape(boundary_map))
         
         return boundary_map
 
@@ -77,7 +93,6 @@ def compute_homology(complex):
             B = boundary_map(complex[i], complex[i-1])
             
             if type(A) == type(0):
-                print("Test")
                 rank_a = 0
                 rank_b = np.linalg.matrix_rank(B)
                 rank_h = rank[i] - rank_a - rank_b
@@ -87,8 +102,7 @@ def compute_homology(complex):
                 rank_b = np.linalg.matrix_rank(B)
                 rank_h = rank[i] - rank_a - rank_b
                 elementary_divisors = find_elementary_divisors(A, rank_a)
-            homology.append((rank_h, elementary_divisors))
-    print(homology)  
+            homology.append((rank_h, elementary_divisors)) 
     return homology
 
 def pretty_print(homology):
@@ -102,30 +116,80 @@ def pretty_print(homology):
         print(text)
     print("For all i greater than " + str(len(homology)) + ", H_i(X) is 0.")
     print("----------------------------------------------------")
-        
 
+# For the specific delta complex, please see Hatcher 2.1.8
+def generate_lens_space(n):
+    if n == 1:
+        input = [[(), ()],
+                  [(1, 0), (1, 1), (0, 0)],
+                  [(0, 0, 1), (0, 2, 0)],
+                  [(0, 1, 1, 0)]]
+    elif n > 1:
+        input = []
+        # There are only 2 points in the Delta Complex
+        # 0 - the point at the center
+        # 1 - the point out at the polygon boundary
+        input.append([(), ()])
+        
+        # Number of edges should be -
+        # n from the boundary connecting to the top
+        # 1 from bottom to top at the center
+        # 1 on the boundary
+        # We will make the list of edges in that order
+        
+        edge_list = []
+        for i in range(n):
+            # Appending n edges from boundary to center
+            edge_list.append((1, 0))
+        # the edge from bottom to top is a loop from 0 to 0
+        # and the edge from boundary to itself
+        edge_list.append((0,0))
+        edge_list.append((1, 1))
+        input.append(edge_list)
+        # Number of Faces should be -
+        # n on the top of the complex (ordered 0, 1, 1 vertex counter-clock wise)
+        # n on the neighbors of the Tetrahedron (ordered 0, 0, 1 vertex counter-clock wise)
+        # We will make the list of faces in that order
+        
+        face_list = []
+        # The vertex of face will be ordered as such:
+        for i in range(n):
+            # Append each face on the top
+            # start at face with left edge - 0, right edge - 1
+            next = int((i + 1) % n)
+            face_list.append((n+1, next, i))
+        for i in range(n):
+            # Append each face in neighbors
+            next = int((i + 1) % n)
+            face_list.append((next, i, n))
+        input.append(face_list)
+        
+        # Number of Tetrahedrons should be n, vertex ordered 0, 0, 1, 1
+        # We will make the list of Tetrahedrons in that order
+        tetrahedron_list = []
+        for i in range(n):
+            next = int((i + 1) % n)
+            tetrahedron_list.append((next, i, n + next, n + i))
+        input.append(tetrahedron_list)   
+    
+    return input
+    
 if __name__ == "__main__":
-    
-    # Real Projective Plane
-    input = [[(), ()],
-             [(1,0), (1,0), (0,0)],
-             [(1,0,2), (0, 1, 2)]]
-    
-    # Torus
-    input = [[()],
-             [(0, 0), (0,0), (0,0)],
-             [(0,1,2), (2, 1, 0)]]
-    
+    input_file = sys.argv[1]
+    if input_file == "lens":
+        num = int(sys.argv[2])
+        input = generate_lens_space(num)
+    else: 
+        input = read_input(input_file)
+
     dimension = len(input)
     f_vector = find_f_vector(input)
     
     print("Dimension of Complex: ", dimension)
     print("F-vector: ", f_vector)
     
-    # boundary_map(input[2], input[1])
-    # boundary_map(input[1], input[0])
-    # boundary_map([], input[0])
-    
     homology = compute_homology(input)
     pretty_print(homology)
+    
+    # generate_lens_space(100)
     
